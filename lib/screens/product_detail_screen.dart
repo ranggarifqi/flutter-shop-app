620cec033df956e7d85806e02df05d8c55944c88
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-07-15 13:50:05 +0700

    Product detail

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -10,18 +10,49 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final String productId = ModalRoute.of(context).settings.arguments;
 
-    final productsData = Provider.of<Products>(
+    final productsProvider = Provider.of<Products>(
       context,
       listen: false,
     );
 
-    final product = productsData.findProductByID(productId);
-
-    print(product.isFavorite);
+    final product = productsProvider.findProductByID(productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '\$${product.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              width: double.infinity,
+              child: Text(
+                '${product.description}',
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
